ad6a639bba6aeb0b36c003460445f944be9d8a8c
ParthSharma05 <144729391+ParthSharma05@users.noreply.github.com>
2025-06-07 10:48:18 +0530

    Initial Commit

---
+++ MoviePlayer/MoviePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviePlayerApp.swift
+//  MoviePlayer
+//
+//  Created by parth sharma on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
